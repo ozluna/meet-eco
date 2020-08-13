801b0cd730b728648480fa57898020c73ddc7c63
Ozge <ozonel883@hotmail.com>
2020-08-13 09:42:00 +0000

    adding the code to the heroku

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import os
-from flask import Flask, render_tempplate, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
+
 
 # creates an instance of flask and assign it to the app variable
 app = Flask(__name__)
 
-MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "meet_event"
-COLLECTION_NAME = "theFirstDB"
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# connecting DB to the app
+app.config["MONGO_DBNAME"] = 'meet_event'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", events=mongo.db.events.find())
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+

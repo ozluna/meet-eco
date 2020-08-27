ceab1dea57a2626161381e26fcb7caa5bba465a5
Ozge <ozonel883@hotmail.com>
2020-08-27 09:43:56 +0000

    double loop problem resolved and trying to add participants to the data base

--- app.py
+++ app.py
@@ -19,36 +19,43 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", events=mongo.db.events.find())
+    ca = list(mongo.db.event_categories.find())
+    the_event = list(mongo.db.events.find())
+    guest = mongo.db.attenders.find()
+    return render_template("index.html",
+                    events=the_event, event_categories=ca, attenders=guest)
 
 
 @app.route("/create_event")
 def create_evet():
-    return render_template("createevent.html")
+    event_cat = mongo.db.event_categories.find()
+    return render_template("createevent.html", event_categories=event_cat)
 
 
 @app.route("/insert_event", methods=["POST"])
 def insert_event():
-    event = mongo.db.events
-    event.insert_one(request.form.to_dict())
+    mongo.db.events.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
 
 
 @app.route("/edit_event/<event_id>")
 def edit_event(event_id):
-    edited_event = mongo.db.events.find_one({"_id": ObjectId(event_id)})
-    return render_template("editevent.html", event=edited_event)
+    events = mongo.db.events.find_one({"_id": ObjectId(event_id)})
+    cats = mongo.db.event_categories.find()
+    return render_template("editevent.html",
+                            event=events, event_categories=cats)
 
 
 @app.route('/update_event/<event_id>', methods=['POST'])
 def update_event(event_id):
     event = mongo.db.events
     event.update({'_id': ObjectId(event_id)},
-    {
+    {        
         'event_name': request.form.get('event_name'),
         'event_description': request.form.get('event_description'),
         'event_date': request.form.get('event_date'),
-        'places': request.form.get('places')
+        'places': request.form.get('places'),
+        'event_pic': request.form.get('event_pic')
     })
     return redirect(url_for('index'))
 
@@ -57,6 +64,13 @@ def update_event(event_id):
 def remove_event(event_id):
     mongo.db.events.remove({'_id': ObjectId(event_id)})
     return redirect(url_for('index'))
+
+
+@app.route('/insert_attenders/<event_id>', methods=['POST'])
+def insert_attenders(event_id):
+    guest = mongo.db.attenders.find({'_id': ObjectId(event_id)})
+    guest.insert_one(request.form.to_dict())
+    return render_template('index.html', attenders=guest)
 
 
 if __name__ == "__main__":
